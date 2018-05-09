b290ab6790d44eb4c2f85e80daee8c635445e4b7
Mars_i <dwellerofuniverse@gmail.com>
2018-05-09 23:02:33 +0800

    修改了一些文件结构

--- RenderSkybox.h
+++ RenderSkybox.h
@@ -1,7 +1,74 @@
-#pragma once
-#include "model.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Shader.h"
+#include "Camera.h"
+#include "Model.h"
+
+#include <stb_image.h>
+
+#include <iostream>
+#include <vector>
+
+unsigned int skyboxVAO;
 
 void RenderSkybox(Shader& skyboxShader) {
+	float skyboxVertices[] = {
+		// positions          
+		-5.0f,  5.0f, -5.0f,
+		-5.0f, -5.0f, -5.0f,
+		5.0f, -5.0f, -5.0f,
+		5.0f, -5.0f, -5.0f,
+		5.0f,  5.0f, -5.0f,
+		-5.0f,  5.0f, -5.0f,
 
+		-5.0f, -5.0f,  5.0f,
+		-5.0f, -5.0f, -5.0f,
+		-5.0f,  5.0f, -5.0f,
+		-5.0f,  5.0f, -5.0f,
+		-5.0f,  5.0f,  5.0f,
+		-5.0f, -5.0f,  5.0f,
+
+		5.0f, -5.0f, -5.0f,
+		5.0f, -5.0f,  5.0f,
+		5.0f,  5.0f,  5.0f,
+		5.0f,  5.0f,  5.0f,
+		5.0f,  5.0f, -5.0f,
+		5.0f, -5.0f, -5.0f,
+
+		-5.0f, -5.0f,  5.0f,
+		-5.0f,  5.0f,  5.0f,
+		5.0f,  5.0f,  5.0f,
+		5.0f,  5.0f,  5.0f,
+		5.0f, -5.0f,  5.0f,
+		-5.0f, -5.0f,  5.0f,
+
+		-5.0f,  5.0f, -5.0f,
+		5.0f,  5.0f, -5.0f,
+		5.0f,  5.0f,  5.0f,
+		5.0f,  5.0f,  5.0f,
+		-5.0f,  5.0f,  5.0f,
+		-5.0f,  5.0f, -5.0f,
+
+		-5.0f, -5.0f, -5.0f,
+		-5.0f, -5.0f,  5.0f,
+		5.0f, -5.0f, -5.0f,
+		5.0f, -5.0f, -5.0f,
+		-5.0f, -5.0f,  5.0f,
+		5.0f, -5.0f,  5.0f
+	};
+	// skybox VAO
+	unsigned int  skyboxVBO;
+	glGenVertexArrays(1, &skyboxVAO);
+	glGenBuffers(1, &skyboxVBO);
+	glBindVertexArray(skyboxVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 }
